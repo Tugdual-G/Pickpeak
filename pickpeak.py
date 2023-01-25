41a54701e0733c7598c103ef2d07d9c4265aa0cf
Tugdual-G <tugducarfantan@gmail.com>
2023-01-25 02:18:11 +0100

    opti

--- pickpeak.py
+++ pickpeak.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import glob
 import numpy as np
 import rasterio as rio
 from rasterio.merge import merge
@@ -7,44 +8,38 @@ import matplotlib.pyplot as plt
 from numba import jit
 
 
-@jit(nopython=True, parallel=True)
-def localmax(R, xs, ys, zs, bbox, origin_idx=False):
+@jit(nopython=True, parallel=True, cache=True)
+def localmax(R, xs, ys, zs, bbox):
     """R , xs, ys and margin should be intergers in index coordinates"""
-    X, Y = np.meshgrid(xs, ys)
-    D = (X - X.T) ** 2 + (Y - Y.T) ** 2
-    x0 = []
-    y0 = []
-    z0 = []
-    or_idx = []
+    idx = np.zeros((len(xs)), dtype=np.int_) - 1
     R = R**2
-    m, n = X.shape
-    left, right, bottom, top = bbox
+    m = len(xs)
+    k = 0
 
     for i in range(m):
         trigg = True
-        for j in range(n):
-            if D[i, j] < R and (xs[i] != xs[j] or ys[i] != ys[j]):
-                if zs[i] < zs[j]:
-                    trigg = False
-                    break
+        for j in range(m):
+            d = (xs[i] - xs[j]) ** 2 + (ys[i] - ys[j]) ** 2
+            if d < R and zs[i] < zs[j]:
+                trigg = False
+                break
 
-        if trigg and (left < xs[i] < right) and (bottom < ys[i] < top):
-            x0 += [xs[i]]
-            y0 += [ys[i]]
-            z0 += [zs[i]]
-            or_idx += [i]
+        if trigg and (bbox[0] < xs[i] < bbox[1]) and (bbox[2] < ys[i] < bbox[3]):
+            idx[k] = i
+            k += 1
 
-    if origin_idx:
-        return or_idx
-    else:
-        return x0, y0, z0
+    return idx[0:k]
 
 
-@jit(nopython=True, parallel=True)
+@jit(nopython=True, parallel=True, cache=True)
 def verylocalmax(z, h):
-    xmax = []
-    ymax = []
-    maxlist = []
+    xmax = np.zeros(int(z.shape[0] * z.shape[1] // 3), dtype=np.int_) - 1
+    ymax = np.zeros(int(z.shape[0] * z.shape[1] // 3), dtype=np.int_) - 1
+    maxlist = np.zeros(int(z.shape[0] * z.shape[1] // 3), dtype=np.double) - 1
+    # xmax = []
+    # ymax = []
+    # maxlist = []
+    q = 0
     for i in range(h, z.shape[0] - h, 2 * h):
         for j in range(h, z.shape[1] - h, 2 * h):
             lmax = z[i, j]
@@ -54,9 +49,14 @@ def verylocalmax(z, h):
                     if lmax < z[i + k, j + l]:
                         lmax = z[i + k, j + l]
                         y0, x0 = i + k, j + l
-            xmax += [x0]
-            ymax += [y0]
-            maxlist += [lmax]
+
+            xmax[q] = x0
+            ymax[q] = y0
+            maxlist[q] = lmax
+            q += 1
+            # xmax += [x0]
+            # ymax += [y0]
+            # maxlist += [lmax]
 
     for i in range(z.shape[0] - h - 1, h, -2 * h):
         for j in range(z.shape[1] - h - 1, h, -2 * h):
@@ -67,39 +67,50 @@ def verylocalmax(z, h):
                     if lmax < z[i + k, j + l]:
                         lmax = z[i + k, j + l]
                         y0, x0 = i + k, j + l
-            xmax += [x0]
-            ymax += [y0]
-            maxlist += [lmax]
-    return np.array(xmax), np.array(ymax), maxlist
+            xmax[q] = x0
+            ymax[q] = y0
+            maxlist[q] = lmax
+            q += 1
+            # xmax += [x0]
+            # ymax += [y0]
+            # maxlist += [lmax]
+
+    xmax = xmax[0:q]
+    ymax = ymax[0:q]
+    maxlist = maxlist[0:q]
+    return (xmax), (ymax), (maxlist)
 
 
 def find_summits(R, z, bbox=None):
     """R must be in index coordinates"""
     h0 = R // 2
     x, y, z = verylocalmax(z, h0)
-    return localmax(R, x, y, z, bbox)
+    return x, y, z, localmax(R, x, y, z, bbox)
 
 
 def main():
-    fname = "BDALTIV2_25M_FXX_0325_6625_MNT_LAMB93_IGN69.asc"
     dpath = "../topo_map/data/data/raster/BDALTIV2_2-0_25M_ASC_LAMB93-IGN69_D085_2021-09-15/BDALTIV2/1_DONNEES_LIVRAISON_2021-10-00008/BDALTIV2_MNT_25M_ASC_LAMB93_IGN69_D085/"
 
-    datasets = rio.open(dpath + fname, crs="eps:2154")
+    f_paths = glob.glob(dpath + "*.asc")
+    f = "BDALTIV2_25M_FXX_0400_6650_MNT_LAMB93_IGN69.asc"
+
+    print(f)
+    datasets = rio.open(dpath + f, crs="eps:2154")
     z = datasets.read(1)
+    z = np.array(z)
     trans = datasets.transform
     dx = trans[0]
 
     h = int(1e3 // dx)
     h *= 1
-    bbox = [h, z.shape[0] - h, h, z.shape[1] - h]
-    x0, y0, z0 = find_summits(h, z, bbox=bbox)
+    bbox = (h, z.shape[0] - h, h, z.shape[1] - h)
+    x0, y0, z0, _ = find_summits(h, z, bbox=bbox)
     importance = np.ones(len(x0))
 
     for i in range(1, 4):
-        idx = localmax(2 * i * h, x0, y0, z0, bbox=bbox, origin_idx=True)
+        idx = localmax(2 * i * h, x0, y0, z0, bbox=bbox)
         importance[idx] = 1 + i
 
-    print(len(x0), len(y0), len(importance))
     x0, y0 = rio.transform.xy(trans, y0, x0)
 
     x, y = np.meshgrid(

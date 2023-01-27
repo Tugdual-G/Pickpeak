171ae3e9650cc9901c0278d3ff8fa34bf85f2e2e
Tugdual-G <tugducarfantan@gmail.com>
2023-01-27 16:58:58 +0100

    debug uneven

--- pickpeak.py
+++ pickpeak.py
@@ -2,12 +2,11 @@
 import numpy as np
 from numba import jit
 from reduceRaster import max_reduce_nodata
+import pdb
 
-
-@jit(nopython=True, parallel=True, cache=True)
+# @jit(nopython=True, parallel=True, cache=True)
 def localmax(R, xs, ys, zs, bbox):
     """R , xs, ys and margin should be intergers in index coordinates"""
-    print(10 * "--")
     idx = np.arange(len(xs) + 1)
     ok_idx = np.zeros((len(xs)), dtype=np.int_) - 1
     R = R**2
@@ -18,16 +17,16 @@ def localmax(R, xs, ys, zs, bbox):
         trigg = True
         for j in range(m):
             d = (xs[i] - xs[j]) ** 2 + (ys[i] - ys[j]) ** 2
-            if d < R:
+            if d < R and 0 < d:
                 # if the summits are of same z, we keep only one (i)
                 # exclude identical points with 0<d
-                if zs[i] < zs[j] and 0 < d:
+                if zs[i] < zs[j]:
                     trigg = False
                     break
                 else:
                     idx[j] = idx[j + 1]
 
-        if trigg and (bbox[0] < xs[i] < bbox[1]) and (bbox[2] < ys[i] < bbox[3]):
+        if trigg and (bbox[0] <= xs[i] < bbox[1]) and (bbox[2] <= ys[i] < bbox[3]):
             ok_idx[k] = i
             k += 1
         i += 1
@@ -37,20 +36,24 @@ def localmax(R, xs, ys, zs, bbox):
     return ok_idx[0:k]
 
 
-def find_summits(R, z, bbox, nodata=-9999):
-    """R must be in index coordinates"""
-    # the max distance possible between two point should
-    # be <= R in the first pass
-    h0 = int(R / np.sqrt(2))
-    if h0 < 2:
-        h0 = 2
-    z0, x, y = max_reduce_nodata(z, h0, nodata)
+def find_summits(h, z, bbox=None, nodata=-9999):
+    """
+    h must be in index coordinates
+    the max distance possible between two summits, is sqrt(2)*h
+    """
+
+    assert isinstance(h, int)
+    if bbox == None:
+        bbox = (0, z.shape[0], 0, z.shape[1])
+
+    R = h * np.sqrt(2)
+    z0, x, y = max_reduce_nodata(z, h, nodata)
     z0.shape = z0.shape[0] * z0.shape[1]
     return z0, x, y, localmax(R, x, y, z0, bbox)
 
 
 def main():
-
+    from matplotlib.patches import Circle
     import rasterio as rio
     from rasterio.merge import merge
     from rasterio.windows import Window
@@ -66,33 +69,44 @@ def main():
     print(f)
     datasets = rio.open(dpath + f, crs="eps:2154")
     z = datasets.read(1)
+    z = z[::, ::]
     # z = np.array(z, dtype=np.double)
 
     m, n = z.shape
-    R = 200
-    h0 = int(R / np.sqrt(2))
-    if h0 < 2:
-        h0 = 2
-
-    x, y = np.meshgrid(np.arange(n), np.arange(m))
-
-    bbox = (0, n, 0, m)
+    h = 100
+    print(f"h0={h}")
+    R = h * np.sqrt(2)
+    print(f"R={R}")
 
     t0 = time.time_ns()
-    zr, x0, y0, idx = find_summits(R, z, bbox)
+    zr, x0, y0, idx = find_summits(h, z)
     t1 = time.time_ns()
 
-    imp = np.zeros_like(x0)
-    imp[idx] = 1
+    imp = np.ones_like(x0)
+    imp[idx] = 2
 
     print("time", (t1 - t0) / 1e6)
 
-    plt.pcolormesh(x, y, z)
-    plt.vlines(np.arange(0, n, h0) - 1 / 2, -1 / 2, m - 1 / 2, color="k")
-    plt.hlines(np.arange(0, m, h0) - 1 / 2, -1 / 2, n - 1 / 2, color="k")
+    x, y = np.meshgrid(np.arange(n), np.arange(m))
+    plt.pcolormesh(x, y, z, cmap="gist_earth")
+    plt.vlines(np.arange(0, n, h) - 1 / 2, -1 / 2, m - 1 / 2, color="k", alpha=0.3)
+    plt.hlines(np.arange(0, m, h) - 1 / 2, -1 / 2, n - 1 / 2, color="k", alpha=0.3)
     plt.scatter(
-        x0, y0, c=imp.tolist(), s=(20 * 2**imp).tolist(), marker="o", cmap="magma"
+        x0,
+        y0,
+        c=imp.tolist(),
+        s=(70 * imp**2).tolist(),
+        marker="+",
+        cmap="RdGy",
+        zorder=10,
     )
+    ax = plt.gca()
+    for xc, yc in zip(x0[idx], y0[idx]):
+        circ = Circle((xc, yc), R, fill=False, edgecolor="k", linestyle="--")
+        ax.add_patch(circ)
+    ax.set_aspect("equal")
+    ax.set_xlim(0, n)
+    ax.set_ylim(0, m)
 
     plt.show()
 
--- reduceRaster.py
+++ reduceRaster.py
@@ -3,11 +3,11 @@ import numpy as np
 from numba import jit
 
 
-@jit(nopython=True, parallel=True, cache=True)
+# @jit(nopython=True, parallel=True, cache=True)
 def max_reduce_nodata(z, h, nodata):
     """Flip Flop"""
 
-    def red(z, h, i_prev0, nodata):
+    def red(z, h, i_prev0, i_prev_m, nodata):
         # This is a reduction fonction,
         # it divide the number of columns by h
         #
@@ -41,36 +41,40 @@ def max_reduce_nodata(z, h, nodata):
                     imax = i
             return vmax, int(imax)
 
+        # i_prev is of shape (i_prev_m, z.shape[1])
+        m0 = i_prev_m
         m = z.shape[0]
-        n = int(z.shape[1] // h)
-        j_in = np.empty(n * z.shape[0], dtype=np.int_)
-        i_prev = np.empty(n * z.shape[0], dtype=np.int_)
-        zr = np.empty((n, z.shape[0]), dtype=np.double)
+        n = z.shape[1] // h
+        j_in = np.empty(n * m0, dtype=np.int_)
+        i_prev = np.empty(n * m0, dtype=np.int_)
+        zr = np.empty((n, m0), dtype=np.double)
         i0 = 0
-        for i in range(m):
+        for i in range(m0):
             for j in range(n):
                 zr[j, i], i0 = maxnan(z[i, j * h : (j + 1) * h], nodata)
-                j_in[j * m + i] = i0 + j * h
-                i_prev[j * m + i] = i_prev0[i * n * h + (j * h + i0)]
+                j_in[j * m0 + i] = i0 + j * h
+                i_prev[j * m0 + i] = i_prev0[i * n * h + (j * h + i0)]
 
         return zr, j_in, i_prev
 
     m, n = z.shape
 
-    idxj = np.empty((n * m), dtype=np.int_)
-    for i in range(m):
-        for j in range(n):
-            idxj[i * n + j] = i
+    m0, n0 = h * (m // h), h * (n // h)
 
-    idxi = np.arange(n // h * m) - 1
-    z, idxj[: n // h * m], _ = red(z, h, idxj, nodata)
-    z, idxi[: (m // h) * (n // h)], idxj[: (m // h) * (n // h)] = red(
-        z, h, idxj, nodata
+    idxj = np.empty((n0 * m0), dtype=np.int_)
+    for i in range(m0):
+        for j in range(n0):
+            idxj[i * n0 + j] = i
+
+    idxi = np.arange(n0 // h * m0)
+    z, idxj[: n0 // h * m0], _ = red(z, h, idxj, m0, nodata)
+    z, idxi[: (m0 // h) * (n0 // h)], idxj[: (m0 // h) * (n0 // h)] = red(
+        z, h, idxj, n0 // h, nodata
     )
     return (
         z,
-        idxj[: (m // h) * (n // h)],
-        idxi[: (m // h) * (n // h)],
+        idxj[: (m0 // h) * (n0 // h)],
+        idxi[: (m0 // h) * (n0 // h)],
     )
 
 
@@ -80,8 +84,8 @@ def test1():
 
     pi = np.pi
     t = 0
-    m, n = 2**10, 2**10
-    h = 2**8
+    m, n = 2**5 + 1, 2**5 + 3
+    h = 2**2
     x, y = np.meshgrid(np.arange(n), np.arange(m))
 
     r = np.sqrt(x**2 + y**2)
@@ -89,6 +93,9 @@ def test1():
 
     r = np.sqrt((x - 2 * n) ** 2 + y**2)
     a += np.sin(pi * 7 * r / m)
+
+    a = a**4
+    a -= np.random.rand(*a.shape)
 
     a[0, 0] = 3
     a[-1, 0] = 3
@@ -118,7 +125,6 @@ def test2():
 
     datasets = rio.open(dpath + f, crs="eps:2154")
     z = datasets.read(1)
-    print(np.isfortran(z))
     z = np.array(z, dtype=np.double)
     z = z[::10, ::10]
     m, n = z.shape
